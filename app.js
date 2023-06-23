4e7fb82c535102dc9995d72e074835efb1584fd1
ppcat123 <96396333+ppcat123@users.noreply.github.com>
2023-06-23 22:27:30 +0800

    app.js

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
-const yourDate = new Date("2022-10-04T00:00:00"),
+const yourDate = new Date("2022-10-04T00:00:00");
+const music = ['1', '2'];
 
-music = ['1', '2'];
+document.addEventListener('DOMContentLoaded', function() {
+  const rootTime = document.querySelector("time");
+  const audioElement = document.querySelector("audio");
+  const maskElement = document.createElement("div");
+  maskElement.id = "mask";
+  document.body.appendChild(maskElement);
 
-document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector("anni").textContent = `${(yourDate.getDate() > 9) ? yourDate.getDate() : "0" + yourDate.getDate()}-${(yourDate.getMonth() > 8) ? (yourDate.getMonth() + 1) : "0" + (yourDate.getMonth() + 1)}-${yourDate.getFullYear()}`;
 
-      var rootTime = document.querySelector("time");
+  document.querySelector("date").textContent = "DAY " + Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24);
 
-      document.querySelector("anni").textContent = `${(yourDate.getDate()>9)?yourDate.getDate():"0"+yourDate.getDate()}-${(yourDate.getMonth()>8)?(yourDate.getMonth()+1):"0"+(yourDate.getMonth()+1)}-${yourDate.getFullYear()}`;
+  function updateTime() {
+    const today = new Date();
+    const hrs = (Math.floor(Math.floor((today - yourDate) / 1000) / 60 / 60)) % 12;
+    const min = (Math.floor(Math.floor((today - yourDate) / 1000) / 60)) % 60;
+    const sec = Math.floor((today - yourDate) / 1000) % 60;
+    rootTime.textContent = `${(hrs > 9) ? hrs : "0" + hrs}:${(min > 9) ? min : "0" + min}:${(sec > 9) ? sec : "0" + sec}`;
+  }
 
-      
+  updateTime();
+  const timer = setInterval(updateTime, 1000);
 
-      document.querySelector("date").textContent = "DAY "+Math.floor( Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24);
+  audioElement.addEventListener("ended", function() {
+    const randomMusicIndex = Math.floor(Math.random() * music.length);
+    audioElement.setAttribute("src", `music/${music[randomMusicIndex]}.mp3`);
+    audioElement.load();
+    audioElement.play();
+  });
 
-      function olock() {
-
-            var today = new Date(),
-
-            hrs = (Math.floor( Math.floor((today - yourDate) / 1000) / 60 / 60)) % 12,
-
-            min = (Math.floor( Math.floor((today - yourDate) / 1000) / 60)) % 60,
-
-            sec =  Math.floor((today - yourDate) / 1000) % 60;
-
-            rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
-
-      } olock();
-
-      var timer = setInterval(function(){olock()}, 1000);
-
-      document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
-
-      document.getElementsByTagName("body")[0].insertAdjacentHTML(
-
-            "beforeend",
-
-            "<div id='mask'></div>"
-
-      );
-
+  const randomMusicIndex = Math.floor(Math.random() * music.length);
+  audioElement.setAttribute("src", `music/${music[randomMusicIndex]}.mp3`);
+  audioElement.load();
+  audioElement.play();
 }, false);
